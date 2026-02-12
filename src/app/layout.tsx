754758df2845f6d4ed820ed369a96cac2df3854d
sarim-alis <sarimslayerali786@gmail.com>
2026-02-12 08:41:54 +0500

    feat: dark mode setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+        suppressHydrationWarning
       >
         <StoreProvider>
           <LayoutContent>{children}</LayoutContent>
